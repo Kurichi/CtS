73f79c173d0f2f80124b7a039e3eed0872c08347
Kurichi <yuuyapp080@gmail.com>
2020-04-14 20:33:35 +0900

    test vuex

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,7 +7,8 @@
 <script lang="ts">
 import { Vue } from 'vue-property-decorator';
 
-export default class App extends Vue {}
+export default class App extends Vue {
+}
 </script>
 
 <style>
---
+++ client/src/store/index.ts
@@ -0,0 +1,10 @@
+import Vuex, { StoreOptions } from 'vuex';
+
+const store: StoreOptions<unknown> = {
+    state: {
+        version: '1.0.0',
+    },
+    modules: {
+
+    },
+};
